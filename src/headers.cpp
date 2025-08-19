200cda131935adad5bb7dac1fe02d84e7ff87510
Evgeniy <btnt51@yandex.ru>
2025-08-19 23:19:20 +0000

    Выполненное задание

--- src/headers.cpp
+++ src/headers.cpp
@@ -1,5 +1,6 @@
 #include "headers.h"
 
+#include <algorithm>
 #include <ranges>
 #include <string_view>
 
@@ -8,14 +9,67 @@ using namespace std::string_view_literals;
 using Callback = std::function<void(std::string_view, std::string_view)>;
 
 
-void iterHeaders(std::string_view req, Callback&& callback) {
-  // code here
+void iterHeaders(std::string_view header, Callback&& callback) {
+    if(header.empty())
+        throw std::runtime_error("Empty header");
+
+    auto lines = header | std::views::split('\r') |
+                 std::views::transform([](auto &&part) { return part | std::views::split('\n'); }) | std::views::join |
+                 std::views::filter([](auto &&line) { return !line.empty(); }) | std::views::drop(1);
+
+    std::ranges::for_each(lines, [&](auto&& line_view){
+        std::string_view line = std::string_view(line_view.begin(), line_view.size());
+
+        const size_t colon = line.find(':');
+        if (colon == std::string_view::npos) {
+            throw std::runtime_error("Invalid header format");
+        }
+        std::string_view field_name = line.substr(0, colon);
+        constexpr std::size_t offset_to_data = 2;
+        std::string_view field_value = line.substr(colon + offset_to_data);
+        callback(field_name, field_value);
+    });
+
 }
 
 std::pair<std::string, std::string> findHostPort(std::string_view req) {
-  // code here
+    try {
+        std::string host, port;
+        iterHeaders(req, [&host, &port](std::string_view field_name, std::string_view field_value) {
+            if (field_name == "Host") {
+                size_t colon_pos = field_value.find(':');
+                if (colon_pos != std::string_view::npos) {
+                    host = std::string(field_value.substr(0, colon_pos));
+                    port = std::string(field_value.substr(colon_pos + 1));
+                } else {
+                    host = std::string(field_value);
+                    port = "80";
+                }
+            }
+        });
+        return {host, port};
+    } catch (std::exception& e) {
+        throw;
+    }
+
 }
 
 std::optional<size_t> findContentLength(std::string_view rsp) {
-  // code here
+    try {
+        std::optional<std::size_t> result;
+        iterHeaders(rsp, [&result](std::string_view field_name, std::string_view field_value) {
+            if(field_name == "Content-Length") {
+                std::size_t value{0};
+                auto res = std::from_chars(field_value.data(), field_value.data() + field_value.size(), value);
+                if(res.ec == std::errc()) {
+                    result = value;
+                } else {
+                    result = std::nullopt;
+                }
+            }
+        });
+        return result;
+    } catch (std::exception& e) {
+        throw;
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,12 @@
 
 #include <string_view>
 #include <iostream>
+#include <print>
 
 using boost::asio::io_service;
 using boost::asio::co_spawn;
 using boost::asio::async_read_until;
+using boost::asio::async_read;
 using boost::asio::awaitable;
 using boost::asio::use_awaitable;
 using boost::system::error_code;
@@ -20,41 +22,102 @@ using boost::asio::buffer;
 using boost::asio::dynamic_buffer;
 using boost::asio::transfer_at_least;
 using boost::asio::ip::tcp;
+using boost::asio::detached;
+using boost::asio::streambuf;
 
 
 constexpr std::string_view delimiter = "\r\n\r\n";
 
+awaitable<void> connect_to_remote_host(std::string_view header_response, tcp::socket &remote_host) {
+    std::string host, port;
+    try {
+        std::tie(host, port) = findHostPort(header_response);
+    } catch (std::exception& e) {
+        std::println("Error occured while parsing header: {}", e.what());
+        co_return;
+    }
+    auto addr = boost::asio::ip::make_address(host);
+    tcp::endpoint endpoint(addr, std::stoi(port));
+    if (not remote_host.is_open())
+        co_await remote_host.async_connect(endpoint, use_awaitable);
 
-awaitable<void> session(tcp::socket client_socket, io_service& io_service)
-{
-  // code here
+    co_await async_write(remote_host, boost::asio::buffer(header_response.data(), header_response.size()), use_awaitable);
 }
+
+awaitable<std::optional<size_t>> read_header(tcp::socket &remote_host, tcp::socket& client_socket) {
+    streambuf buf;
+    std::size_t header_size = co_await async_read_until(remote_host, buf, delimiter, use_awaitable);
+    auto bufs = buf.data();
+    std::string content(boost::asio::buffers_begin(bufs), boost::asio::buffers_begin(bufs) + buf.size());
+    std::string header_response = content.substr(0, header_size);
+    std::optional<std::size_t> cl;
+    try {
+        cl = findContentLength(header_response);
+    } catch (std::exception& e) {
+        std::println("Error occured while parsing header: {}", e.what());
+        co_return std::nullopt;
+    }
+    co_await async_write(client_socket, buffer(buf.data(), buf.size()), use_awaitable);
+    if(cl and header_response.size() < buf.size()) {
+        cl.value() -= buf.size()-header_response.size();
+    }
+    co_return cl;
+}
+
+
+
+awaitable<void> pipe(tcp::socket &client_socket, tcp::socket &remote_host, std::size_t remaining) {
+    std::size_t current_read{0};
+    while(current_read < remaining) {
+        std::string content_from_remote;
+        content_from_remote.resize(remaining - current_read);
+        std::size_t readed_data = co_await async_read(remote_host, buffer(content_from_remote), use_awaitable);
+        std::string_view content2{content_from_remote.data(), readed_data};
+        current_read += readed_data;
+        std::println( "{}", content2);
+        co_await async_write(client_socket, buffer(content_from_remote, readed_data), use_awaitable);
+    }
+}
+
+awaitable<void> session(tcp::socket client_socket, io_service& io_service) {
+    tcp::socket remote_host(io_service);
+    for (;;) {
+        streambuf buf;
+        std::size_t header_size = co_await async_read_until(client_socket, buf, delimiter, use_awaitable);
+        auto bufs = buf.data();
+        std::string content(boost::asio::buffers_begin(bufs), boost::asio::buffers_begin(bufs) + buf.size());
+        std::string header_request = content.substr(0, header_size);
+
+        co_await connect_to_remote_host(header_request, remote_host);
+        auto remaining = co_await read_header(remote_host, client_socket);
+        if (remaining) {
+            co_await pipe(client_socket, remote_host, remaining.value());
+        }
+    }
+}
+
 
 class Server
 {
 public:
-  Server(io_service& io_service, short port)
+    Server(io_service& io_service, short port)
     : io_service_(io_service)
     , acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
-    , socket_(io_service)
-  {
-    do_accept();
-  }
+    {
+        co_spawn(io_service_, do_accept(), detached);
+    }
 
 private:
-  void do_accept()
-  {
-    acceptor_.async_accept(socket_,
-      [this](error_code ec)
-      {
-        // code here
+    awaitable<void> do_accept()
+    {
+      while(true) {
+          auto socket = co_await acceptor_.async_accept(use_awaitable);
+          co_spawn(io_service_.get_executor(), session(std::move(socket), io_service_), detached);
       }
-    );
-  }
+    }
 
-  io_service& io_service_;
-  tcp::acceptor acceptor_;
-  tcp::socket socket_;
+    io_service& io_service_;
+    tcp::acceptor acceptor_;
 };
 
 int main(int argc, char* argv[]) {
